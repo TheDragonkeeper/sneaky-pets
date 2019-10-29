225f5366f68426b1ab02157eb19d970c283bd4ab
ryan <ryan@nixheads.co.uk#>
2019-10-29 11:15:25 +0000

    multitraded and load pet

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
+#!/usr/bin/python3
 import os,sys,time
+import os.path
 sys.path.append('scripts')
 import setup,gui
+import threading
+import json
+from threading import Timer
 os.system('clear')
 
-pet = setup.startsetup()
+pet = ""
+if os.path.isfile("pet.json") == True:
+	with open('pet.json', "r") as json_file:
+		pet = json.load(json_file)
+else:
+	pet = setup.startsetup()
+	with open("pet.json", "w") as write_file:
+		json.dump(pet, write_file)
 
-while True:
-	gui.refresh(pet,pet["currentemo"])
+
+question = 0
+def guiloop():
+	global question
+	while True:
+		gui.refresh(pet,pet["currentemo"],question)
+def resetinput():
+	global question
+	while True:
+		time.sleep(20)
+		userinputthread.terminate()
+		userinputthread.start()
+		question = 0
+
+def userinput():
+	global question
+	while True:
+		waitingoninput = input("")
+		question = gui.menuinput(waitingoninput)
+	
+threads = []
+
+
+guithread = threading.Thread(target=guiloop)
+threads.append(guithread)
+
+userinputthread = threading.Thread(target=userinput)
+threads.append(userinput)
+
+resetinputthread = threading.Thread(target=resetinput)
+threads.append(resetinputthread)
+
+guithread.start()
+userinputthread.start()
+resetinputthread.start()
--- scripts/gui.py
+++ scripts/gui.py
@@ -1,8 +1,18 @@
 import time,os
 
-def refresh(pet, emotion):
+def refresh(pet, emotion, question):
 	for frame in pet[emotion]:
 		print("Health: "+str(pet["health"])+"/"+str(pet["maxhealth"])+" "*10+str(pet["name"])+" "*10+"Hunger: "+str(pet["hunger"])+"/"+str(pet["maxhunger"])+"\n")
 		print("".join(frame))
+		if question == 0:
+			print("\nM: Menu")
+		else:
+			print(str(question))
 		time.sleep(1)
 		os.system('clear')
+
+def menuinput(userinput):
+	question = 0
+	if userinput == "m" or userinput == "M":
+		question = "F: Feed  P: Play  S: Sleep"
+	return question
